6d64b3d586258db4ef061f95b108df9aaafdf913
Jason Korol <ejkorol@gmail.com>
2024-09-23 20:39:50 -0400

    integrate navbar into layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,21 @@
 import "./globals.css";
-import {Providers} from "./providers";
+import { Providers } from "@/providers/Providers";
+import NavBar from "@/components/NavBar/NavBar";
 
-export default function RootLayout({children}: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang="en" className='dark'>
+    <html lang="en" className="dark">
       <body>
         <Providers>
-          {children}
+          <NavBar />
+          <main className="mt-32 px-8">{children}</main>
         </Providers>
       </body>
     </html>
   );
 }
+
