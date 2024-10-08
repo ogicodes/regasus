8021fbef658e750348b71c0f159e88cb237b645b
ogicodes <ogi@ogidesigns.ca>
2024-10-08 01:56:48 -0400

    created carousel component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
 import Hero from "@/components/Hero/Hero";
 import PickerGallery from "@/components/PickerGallery/PickerGaller";
 import WhyUs from "@/components/WhyUs/WhyUs";
+import Carousel from '@/components/Carousel/Carousel'
+import { EmblaOptionsType } from 'embla-carousel'
+
+const OPTIONS: EmblaOptionsType = { dragFree: true, loop: true }
+const SLIDE_COUNT = 5
+const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
 
 export default function Home() {
 
   return (
     <section className="flex flex-col items-center justify-center">
       <Hero />
+      <Carousel slides={SLIDES} options={OPTIONS} />
       <WhyUs />
       <PickerGallery />
     </section>
